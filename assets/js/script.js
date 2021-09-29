c568dadb9958dc3e88653102a5139f8c5bf373c0
MJGR <mjgriley@gmail.com>
2021-09-29 11:42:11 -0400

    APIKey working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var MSAPIKey = '71dcc5160836657f52acf194332c63da'
-var ticker = "AAPL,MSFT"
-
-    
-    var qDated = "http://api.marketstack.com/v1/eod/latest?access_key=" + MSAPIKey + "&data_from=2020-02-27&symbols=" + ticker
-
+var ticker = "AAPL,MSFT,FB,TSLA" //These are the temp stock tickers, tickerInput blow is correct
+var sBtn = $('#submit')
+sBtn.click(pullHData)
+var tickerInput = $('#tickerSearch').val()
+function pullHData() {//This function only pulls historical data only from 2020-20-28
+    var qDated = "http://api.marketstack.com/v1/eod/2020-02-28?access_key=" + MSAPIKey + "&symbols=" + ticker
     fetch(qDated,{
         cache: 'reload',
     })
@@ -13,3 +14,4 @@ var ticker = "AAPL,MSFT"
     .then(function (data) {
         console.log(data)
     })
+}
