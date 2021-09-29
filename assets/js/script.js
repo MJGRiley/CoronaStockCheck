85ff7a406171d8ebb64dc3caf260e03d8043787b
Giovanni Malcolm <giovannimalcolm@giovannis-mbp.lan>
2021-09-29 11:33:53 -0400

    function set up

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,38 @@ var ticker = "AAPL,MSFT"
     .then(function (data) {
         console.log(data)
     })
+
+// CHART .JS ///
+
+// DATA
+var stars = [135850, 52122]; //y-axis labels. need a function to pull Q1 2020 stock high
+var frameworks = ['COVID-19 Crash', 'Today']; /// x-axis labels
+
+var chart = document.getElementById('compChart');
+
+//creating the BAR chart.
+var compChart = new Chart(chart, {
+    type: 'bar',
+    data: {
+        labels: frameworks,
+        datasets: [{ 
+            label: "Coronavirus Stock Valuation vs. Current",
+            data: stars
+        }]
+
+    }
+ }
+)
+
+//need a function to go thru API output object and pull Q1 2020 lowest close for a particular ticker.
+//function will: take ticker input -> use input to call API data -> index thru API data to create array of closing prices between 1/1/2020 - 4/1/2020 ->
+// use a method to get the lowest close value and the index of that value in the array. use index to get date of that closing value.
+// make an object/array to pull dates (text content for graph) between the Q1/2020 time range from
+//pull most recent stock close from API for other bar. most recent instead of today because user may use this app on a fed holiday or weekend
+//import data into compChart
+//"tickersearch" is input ID
+
+function q1Low () {
+
+
+}
